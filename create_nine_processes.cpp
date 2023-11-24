06e6d6a464d525ee7b972d1171b6f91e8d1ffdf5
Pavan Kumar S <53817263+P4v4n5@users.noreply.github.com>
2023-11-24 02:01:22 -0800

    Update create_nine_processes.cpp

--- create_nine_processes.cpp
+++ create_nine_processes.cpp
@@ -2,21 +2,24 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int main() {
-    printf("original process is %d\n", getpid());
+int main() 
+
+{
+    printf("original process is %d\n", getpid()); // its Parent is not considered as per nine processes creation as per assignment requirement
     pid_t pid;
     
-    pid = fork(); // creates 2 processes 1 child 1 parent
-    pid = fork(); // creates 4 processes 2 child 2 parent
+    pid = fork(); // creates 2 processes with 1 child & 1 parent
+    pid = fork(); // creates 4 processes with 2 child & 2 parent
 
     // only the child processes should execute
     // hence only 2 child processes enter the if section
-    if (pid == 0) {
-        fork();  // total 6 processes, 2 child 2 parent 
-        fork();  // total 10 processes, 2 child 2 parent 
+    if (pid == 0) // if pid is 0, it indicates its a child process
+    {
+        fork();  // total 6 processes with 2 child & 2 parent 
+        fork();  // total 10 processes with 2 child & 2 parent 
     } 
 
-    printf("I am %d and my parent is %d\n", getpid(), getppid());
+    printf("Child --> %d | Parent --> %d\n", getpid(), getppid());
 
     while (wait(NULL) > 0) {} // wait for each child process
 
