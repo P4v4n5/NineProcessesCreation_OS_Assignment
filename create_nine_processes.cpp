858303e720917a214cff4793bd8e8735e1ad57e3
Pavan Kumar S <53817263+P4v4n5@users.noreply.github.com>
2023-11-24 01:55:25 -0800

    Update create_nine_processes.cpp

--- create_nine_processes.cpp
+++ create_nine_processes.cpp
@@ -2,18 +2,6 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-// Author : Divya Meharwade
-// Below code displays the pid of the current process
-// And forks 9 processes as defined in the assignment.
-// No parent process has more than 2 child processes.
-// Initial 2 forks create 4 processes
-// only 2 child processes enter the if section
-// firts fork on line 23 - create 2 additional processes 
-// resulting in 6 total processes.
-// fork on line 24 is executed by all 4 process and hence
-// 4 additional processes are created resulting in 10.
-// 9 new child processes and 1 original parent process
-
 int main() {
     printf("original process is %d\n", getpid());
     pid_t pid;
